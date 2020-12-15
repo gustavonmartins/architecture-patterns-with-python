11eb90b5c6877aa6418a476da8ce69f206eedabe
Sarra <martins.yo@gmail.com>
2020-12-15 22:44:37 +0100

    reorganisation

--- test_orm.py
+++ test_orm.py
@@ -1,13 +1,16 @@
-import models
+from db import create_db_and_mapper
 
 
 def test_mapping():
+    import models
     from repository import OrderLineRepository
-    from db import create_db_and_mapper
 
     """Tests if SQL Alchemy traditional mapping was done properly by checking if file inserted into DB really corresponds to target model"""
 
+    # TODO: create and map should not be here!
     create_db_and_mapper()
+    ##
+
     myorderline = models.OrderLine(sku="blue t shirt", qty=20, ref="ordereed from king")
     repo = OrderLineRepository()
     repo.add(myorderline)
